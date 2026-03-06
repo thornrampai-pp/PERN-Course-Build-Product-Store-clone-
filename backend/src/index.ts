c5dc489470c3935f29002e97886dfc9a0319c4f4
thornrampai <thornrampai@nicetynine.com>
2026-03-06 20:47:05 +0700

    [Add] setup project

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+
+import express from 'express';
+import cors from 'cors'
+import { ENV } from "./config/env"; 
+import { clerkMiddleware } from '@clerk/express'
+
+const app = express()
+// middleware request coolies and header for jwt (auth obj wil be attached to the req)
+app.use(cors({origin: ENV.FRONTEND_URL}));
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({extended:true})); 
+
+// corss(cross-origin resource sharing) : browse security rule
+
+
+app.get("/",(req,res)=>{
+  res.json({
+    message: "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+    endpoint:{
+      users: "/api/users",
+      products:"/api/products",
+      comments: "/api/comments"
+    }
+  });
+});
+
+app.listen(ENV.PORT,()=>{
+  console.log("Server is up and running on PORT:",ENV.PORT);
+})
