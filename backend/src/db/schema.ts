b20d85579f855767b9b4840068d0e3d99ec639b7
thornrampai <thornrampai@nicetynine.com>
2026-03-06 21:59:01 +0700

    [update] feild cupdateAt

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -9,7 +9,7 @@ export const users = pgTable("users",{
   name: text("name"),
   imageUrl: text("image_url"),
   createdAt: timestamp("created_at",{mode:"date"}).notNull().defaultNow(),
-  updatedAt: timestamp("updated_at",{mode:"date"}).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at",{mode:"date"}).notNull().defaultNow().$onUpdate(()=> new Date()),
 });
 
 export const products = pgTable("products",{
